e598bf65d5dcfbdc8f564e40e9873711d5149494
Cahleeb <78446583+Caleb-Parks@users.noreply.github.com>
2024-04-30 12:33:12 -0400

    Fixed dark mode on EditProfile page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "61ef759c4a3c2ff5bd2fb777f141b66b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "66450c7396d779cef85ee4c9ed54cd4c",
-"/": "66450c7396d779cef85ee4c9ed54cd4c",
-"main.dart.js": "ec0fd9d2d3b30179b2bff72ceded9eb1",
+"index.html": "e280807b31052a9158681327178fe112",
+"/": "e280807b31052a9158681327178fe112",
+"main.dart.js": "87789798e4e903811eaa5002b61775b4",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "308741d4e5b4c74b9380cda7bb0aedde"};
 // The application shell files that are downloaded before a service worker can
