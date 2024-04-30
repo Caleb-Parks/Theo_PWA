a2f0992ffd3625a334de82099f7ac1484dd82857
Cahleeb <78446583+Caleb-Parks@users.noreply.github.com>
2024-04-30 11:39:43 -0400

    Made several pages scrollable and fixed some spacing for smaller screens

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "61ef759c4a3c2ff5bd2fb777f141b66b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b07c6de5fc513bd284549b3cd80c1507",
-"/": "b07c6de5fc513bd284549b3cd80c1507",
-"main.dart.js": "e1ab14437338c7e4b61d276c86df787a",
+"index.html": "75660702d35280dc5c41b1fba550021e",
+"/": "75660702d35280dc5c41b1fba550021e",
+"main.dart.js": "30fed9a69560140967e099df4300c638",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "308741d4e5b4c74b9380cda7bb0aedde"};
 // The application shell files that are downloaded before a service worker can
